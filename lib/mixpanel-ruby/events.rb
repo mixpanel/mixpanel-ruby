58a17e10b99b8e3e231966654eb6b1517e7f9630
Mark Wise <markmediadude@gmail.com>
2015-02-23 15:33:12 -0600

    addd Mixpanel::Events#track_batch to support batch requests

--- lib/mixpanel-ruby/events.rb
+++ lib/mixpanel-ruby/events.rb
@@ -112,5 +112,61 @@ module Mixpanel
 
       @sink.call(:import, message.to_json)
     end
+
+    # Notes that a batch of events has occurred for a distinct_id
+    # Events should be passed as an array, with each element either a
+    # Hash or a string. Each Hash element should have a single key (the event name,
+    # as a string) with the value a Hash of properties. Each string element
+    # will be interpreted as an event name with no properties.
+    #
+    #     tracker = Mixpanel::Tracker.new
+    #
+    #     tracker.track_batch("12345", [
+    #     "Signup Begin",
+    #     {
+    #       "Signup Complete" => {
+    #         'User Sign-up Cohort' => 'July 2013'
+    #       }
+    #     },
+    #     {
+    #       "Welcome Email Sent" => {
+    #         'Email Template' => 'Pretty Pink Welcome',
+    #         'User Sign-up Cohort' => 'July 2013'
+    #       }
+    #     }])
+    def track_batch(distinct_id, events, ip=nil)
+      data = events.map do |event_name_or_hash|
+        event = event_name_or_hash
+        properties = {}
+
+        if event_name_or_hash.is_a?(Hash)
+          event_data = event_name_or_hash.flatten
+          event = event_data[0]
+          properties = event_data[1]
+        end
+
+        properties = {
+          'distinct_id' => distinct_id,
+          'token' => @token,
+          'time' => Time.now.to_i,
+          'mp_lib' => 'ruby',
+          '$lib_version' => Mixpanel::VERSION
+        }.merge(properties)
+
+        properties['ip'] = ip if ip
+
+        {
+          'event' => event,
+          'properties' => properties
+        }
+      end
+
+      data.each_slice(50) do |slice|
+        message = { 'data' => slice }
+
+        @sink.call(:event, message.to_json)
+      end
+    end
+
   end
 end
--- spec/mixpanel-ruby/events_spec.rb
+++ spec/mixpanel-ruby/events_spec.rb
@@ -50,5 +50,76 @@ describe Mixpanel::Events do
         }
     } ]])
   end
+
+  it 'should send a well formed batch track/ message' do
+    events = [
+      { 'Test Event' => {
+          'Circumstances' => 'During a test'
+      }},
+      { 'Other Event' => {
+          'Circumstances' => 'During a different test'
+      }},
+      "Event Without Properties"
+    ]
+    @events.track_batch('TEST ID', events)
+
+    expect(@log).to eq([[:event, 'data' => [
+      {
+        'event' => 'Test Event',
+        'properties' => {
+            'distinct_id' => 'TEST ID',
+            'token' => 'TEST TOKEN',
+            'time' => @time_now.to_i,
+            'mp_lib' => 'ruby',
+            '$lib_version' => Mixpanel::VERSION,
+            'Circumstances' => 'During a test',
+        }
+      },
+      {
+        'event' => 'Other Event',
+        'properties' => {
+            'distinct_id' => 'TEST ID',
+            'token' => 'TEST TOKEN',
+            'time' => @time_now.to_i,
+            'mp_lib' => 'ruby',
+            '$lib_version' => Mixpanel::VERSION,
+            'Circumstances' => 'During a different test',
+        }
+      },
+      {
+        'event' => 'Event Without Properties',
+        'properties' => {
+            'distinct_id' => 'TEST ID',
+            'token' => 'TEST TOKEN',
+            'time' => @time_now.to_i,
+            'mp_lib' => 'ruby',
+            '$lib_version' => Mixpanel::VERSION
+        }
+      }
+    ]]])
+  end
+
+  it 'should send track/ messages in batches of 50' do
+    events = Array.new(75, "Some Event")
+    batches = 75.times.map do |i|
+      {
+        'event' => 'Some Event',
+        'properties' => {
+            'distinct_id' => 'TEST ID',
+            'token' => 'TEST TOKEN',
+            'time' => @time_now.to_i,
+            'mp_lib' => 'ruby',
+            '$lib_version' => Mixpanel::VERSION
+        }
+      }
+    end
+
+    @events.track_batch('TEST ID', events)
+    expect(@log).to eq([
+      [:event, { 'data' => batches[0, 50] }],
+      [:event, { 'data' => batches[50, 25] }]
+    ])
+  end
+
 end
 
