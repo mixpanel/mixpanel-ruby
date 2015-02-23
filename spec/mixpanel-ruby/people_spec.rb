01e09ca4ee9c01babe049d0b5a8b733c69fab3d3
Mark Wise <markmediadude@gmail.com>
2015-02-23 15:33:21 -0600

    added Mixpanel::People#batch to support batch operations

--- spec/mixpanel-ruby/people_spec.rb
+++ spec/mixpanel-ruby/people_spec.rb
@@ -200,4 +200,82 @@ describe Mixpanel::People do
     }]])
   end
 
+  describe "batch operations" do
+
+    it 'should send a well formed batch engage/set message' do
+      @people.batch([{
+          "TEST ID" => {
+              '$set' => {
+                  '$firstname' => 'David',
+                  '$lastname' => 'Bowie',
+              },
+              '$unset' => ['Levels Completed']
+          }
+      }])
+      expect(@log).to eq([
+        [:profile_update, 'data' => [
+          {
+              '$token' => 'TEST TOKEN',
+              '$time' => @time_now.to_i * 1000,
+              '$distinct_id' => 'TEST ID',
+              '$set' => {
+                  '$firstname' => 'David',
+                  '$lastname' => 'Bowie'
+              }
+          },
+          {
+              '$token' => 'TEST TOKEN',
+              '$time' => @time_now.to_i * 1000,
+              '$distinct_id' => 'TEST ID',
+              '$unset' => ['Levels Completed']
+          },
+        ]
+      ]])
+    end
+
+    it 'should send batch engage/ message in batches of 50' do
+      profile_updates = 75.times.map do |i|
+        { "TEST ID" => { '$set' => { "prop_#{i}" => 'David' } } }
+      end
+
+      batches = 75.times.map do |i|
+        {
+            '$token' => 'TEST TOKEN',
+            '$distinct_id' => 'TEST ID',
+            '$time' => @time_now.to_i * 1000,
+            '$set' => { "prop_#{i}" => 'David' }
+        }
+      end
+
+      @people.batch(profile_updates)
+      expect(@log).to eq([
+        [:profile_update, 'data' => batches[0, 50]],
+        [:profile_update, 'data' => batches[50, 25]]
+      ])
+    end
+
+    it 'should reject invalid operations' do
+      @people.batch([{
+          'TEST ID' => {
+              '$set' => {
+                  '$firstname' => 'David',
+                  '$lastname' => 'Bowie',
+              },
+              '$foo' => {
+                  '$bar' => 'baz',
+              }
+          }
+      }])
+      expect(@log).to eq([[:profile_update, 'data' => [{
+          '$token' => 'TEST TOKEN',
+          '$distinct_id' => 'TEST ID',
+          '$time' => @time_now.to_i * 1000,
+          '$set' => {
+              '$firstname' => 'David',
+              '$lastname' => 'Bowie'
+          }
+      }]]])
+    end
+  end
+
 end
